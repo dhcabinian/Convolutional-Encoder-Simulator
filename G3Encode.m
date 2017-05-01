31bbd7f8c6fedd1ab34c0d6573d6c3a42f1a95f3
Bryce Rich <brycerich17@yahoo.com>
2017-04-30 22:42:22 -0400

    Fixed

--- G3Encode.m
+++ G3Encode.m
@@ -1,9 +1,9 @@
-function [output, output_state] = G3Encode(input_state, message_bit)
+function [output, output_state] = G3Encode(input_state, message_bits)
     %State in vector form [m_i, m_i-1, m_i-2]
-    output_state = [message_bit input_state(1:end-1)];
-    c_1 = message_bit; % not certain if this is equivalent to unchecked (0)
+    output_state = [message_bits(1) input_state(1:end-1)];
+    c_1 = message_bits(2); % not certain if this is equivalent to unchecked (0)
     c_2 = mod(output_state(2),2);
-    c_3 = mod(output_state(2) + output_state(3),2);
+    c_3 = mod(output_state(1) + output_state(3),2);
     output = [c_1 c_2 c_3];
     return
 end
